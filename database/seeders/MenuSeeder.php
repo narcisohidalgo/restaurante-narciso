7a5f10f5aa8be3e5f902326a43b64a289dca9077
narcisohidalgo <nhh0002@alu.medac.es>
2023-03-07 00:58:51 +0100

    actualizar menu

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\menu;
+use App\Models\Menu;
 use Database\Factories\MenuFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
